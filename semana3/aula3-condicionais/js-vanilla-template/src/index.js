076097bcdea581c12594f4c3b1462591708c14f9
ThalesMilanezi <milanezitf@gmail.com>
2020-01-29 17:00:02 -0300

    exercícios da tarde do dia 29/01

---
+++ semana3/aula3-condicionais/js-vanilla-template/src/index.js
@@ -0,0 +1,71 @@
+/* Exercícios de interpretação --1--
+1- O código recebe um numero do usuário atraves do prompt e com esse numero,
+ele verifica se o numero digitado pelo usuário recebe a condição de passou no teste ou não passou no teste.
+Teste de verificar se o numero é par ou impar, atraves da verificação do resto da divisão por 2.
+Os numeros pares recebem a mensagem "passou no teste" os numeros impares recebem a mensagem "não passou no teste"]
+*/
+
+/*Exercícios de interpretação --2--
+a) o código acima serve como um sistema para saber o preço das frutas do supermercado
+b) se a fruta for maça a mensagem impressa será "O preço da fruta Maça é R$ 2.25"
+c)Pagaria R$ 24,55 porque 2 laranjas custam 7 reais, uma maça 2.25, 3 bananas 15 reais( fruta que não esta na lista custa 5 reais), uma uva 0.30.
+d) Acredito que seria 5 reais porque sem o break que significa parar o codigo para naquele local ele rodaria até o default que é 5 reais
+*/
+
+/* Exercícios de interpretação --3--
+a mensagem que o código mandará é o numero1 é menor ou igual ao 2
+Acredito que não haverá erro, mesmo que o let mensagem tenha sido declarado dentro do bloco do if(acredito que incorretamente porque ele não faz parte do global assim)
+esse bloco é o bloco paientão ele existe dentro desse escopo. Como foi dito na aula tambem e testado o javascript ele é bem permissivo
+ e isso é bom em algumas horas e ruins em outras horas e acredito que nesse exemplo seja ruim
+*/
+
+/* Exercícios de escrita de código */
+
+/* Exercício -- 4 -- */
+//a)
+/*
+const num1 = prompt("Digite um número")
+const num2 = prompt("Digite outro numero")
+
+if(num1>num2){
+    console.log(num1, num2)
+} if(num2>num1){
+    console.log(num2, num1)
+}*/
+//Não houve nenhuma mensagem de erro mas também ele não imprimiu nenhum numero.
+
+//b)
+/* 
+const num1 = Number(prompt("Digite um número"))
+const num2 = Number(prompt("Digite mais um número"))
+const num3 = Number(prompt("Digite o último numero"))
+
+if (num1 > num2 && num2 > num3) {
+    console.log(num1, num2, num3)
+} 
+if (num2 > num3 && num3 > num1) {
+    console.log(num2, num3, num1)
+} 
+else {
+    console.log(num3, num1, num2)
+} 
+*/
+//resposta do b) se colocar os 3 numeros iguais ele imprime os 3 numeros agora.
+//c)
+/*
+const num1 = Number(prompt("Digite um número"))
+const num2 = Number(prompt("Digite mais um número"))
+const num3 = Number(prompt("Digite o último numero"))
+
+if (num1===num2 && num2===num3){
+    console.log("não pode digitar 3 numeros iguais amigx, insira um numero diferente")
+}
+*/
+/*Exercício 5 
+a)
+https://1drv.ms/u/s!Ah-UdkHhUku0ga10-vJeCSQM6liqjQ?e=z7JT5d
+*/
+//b)
+
+
+

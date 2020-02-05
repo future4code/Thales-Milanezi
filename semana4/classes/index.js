112d118cf43a13a47b50e6fd39ce39a9d92ac8c0
ThalesMilanezi <milanezitf@gmail.com>
2020-02-05 16:17:32 -0300

    o exercício 3

---
+++ semana4/classes/index.js
@@ -0,0 +1,21 @@
+class Post {
+    constructor (titulo, autor, texto){
+        this.titulo = titulo
+        this.autor = autor
+        this.texto = texto
+    }
+}
+let arrayPost =[]
+function imprimirPost(){
+        event.type === "click"
+        let valorTitulo = document.getElementById("titulos").value
+        let valorAutor = document.getElementById("autores").value
+        let valorNewPost = document.getElementById("areadotexto").value
+        let publicar = new Post (valorTitulo, valorAutor, valorNewPost)
+
+        console.log(publicar)
+        arrayPost.push(publicar)
+
+        console.log(arrayPost)
+
+    }

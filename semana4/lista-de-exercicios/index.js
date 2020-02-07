48cab879a589492668c0aea5019917304dfb8e41
ThalesMilanezi <milanezitf@gmail.com>
2020-02-07 10:48:53 -0300

    exercicio 2 de interpretação realizado

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -5,6 +5,14 @@ e o usuário deve digitar um valor da cotação do dolar e esse valor tem que se
 então vai retornar uma string "R$" mais o valor da cotação que o usuario digitar e vai fazer vezes 100
 meu dinheiro vai ser o valor dessa conversão
 
+2-) o Exercicio 2 é um switch case que o usuario vai escolher o tipo de investimento que ele vai querer
+e então o valor será uma conversao do investimento escolhido
+Primeiro foi criado uma function que recebeu 2 paramentos e uma variavel
+o switch foi criado em 4 casos de escolha  e um default caso nenhum dos casos forem selecionados
+o retorno será valoraposInvestimento que é depois da escolha do investimento e a conversão
+logo apos a função foi feito 2 nosvas constantes para serem impresas no console
+O novoMontante será com o valor de 165
+o segundoMontante será com o valor default porque não existe o case tesouro direto
 
 
 

4dbed8bbee0fb89c167c2e40eb52c1d181e8eef5
ThalesMilanezi <milanezitf@gmail.com>
2020-02-07 10:40:58 -0300

    exercicio de interpretação 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,16 @@
+/* Exercícios de interpretação de código
+1-) A função do exercício 1 realiza a conversão de moeda para dolares.
+Então foi feito uma função em que o usuário recebe uma caixa de texto pelo comando prompt
+e o usuário deve digitar um valor da cotação do dolar e esse valor tem que ser numérico
+então vai retornar uma string "R$" mais o valor da cotação que o usuario digitar e vai fazer vezes 100
+meu dinheiro vai ser o valor dessa conversão
+
+
+
+
+
+
+
+
+
+*/

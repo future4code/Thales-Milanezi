53c832d5068fe5275ffb725b5b4ad02d04c9b10d
ThalesMilanezi <milanezitf@gmail.com>
2020-04-20 22:23:28 -0300

    exercicio 1 de typescript completo

---
+++ semana13/Exercicios-Introducao-Typescript/build/exercicios1.js
@@ -0,0 +1,18 @@
+function matematica(num1, num2) {
+    const soma = num1 + num2;
+    const subtracao = num1 - num2;
+    const multiplicacao = num1 * num2;
+    let resultado;
+    if (resultado === num1 > num2) {
+        console.log(`O numero ${num1} é maior que o numero ${num2}`);
+    }
+    else {
+        console.log(`O numero ${num2} é maior que o numero ${num1}`);
+    }
+    console.log("A soma desses dois numeros é", soma);
+    console.log("A subtração desses dois numeros é", subtracao);
+    console.log("A multiplicação desses dois numeros é", multiplicacao);
+    console.log(resultado);
+    console.log(matematica(8, 12));
+}
+//# sourceMappingURL=exercicios1.js.map
---
+++ semana13/Exercicios-Introducao-Typescript/src/exercicio2.ts
@@ -0,0 +1,14 @@
+type numeros = {
+  num1: number,
+  num2: number,
+  num3: number
+
+};
+
+type ObjectNumbers ={
+  quantidadeDeNumeros: number,
+  numerosImpares:number,
+  soma:number
+}
+
+functions numeros(arrays:)

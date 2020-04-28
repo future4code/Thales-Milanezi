fb02acb5de443aa922ecb6c086de55fccf96e120
ThalesMilanezi <milanezitf@gmail.com>
2020-04-28 11:54:30 -0300

    trabalho de classes typescript

---
+++ semana14/Classes/boilerplate_classes/src/UserAccount.ts
@@ -0,0 +1,32 @@
+import {Transactions} from "./Transaction"
+
+export class UserAccount {
+  private balance: number
+  private cpf: string
+  private name: string
+  private age: number
+  private transactions: Transactions[]
+
+  constructor(balance: number, cpf: string, name: string, age: number, transactions: Transactions) {
+    this.balance= balance
+    this.cpf = cpf
+    this.name = name
+    this.age = age
+    this.transactions[] = transactions
+  }
+
+
+
+
+public getBalance(balance: number){
+
+}
+
+public addBalance(): void{
+
+}
+
+
+}
+
+const Atualbalance: UserAccount = new balance('JSONFileManager.ts');
